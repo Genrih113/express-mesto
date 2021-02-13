21ec890f4a84de22cf86aa0764a3ca95cb3ee115
Gennadij Tolstov <genrih113@yandex.ru>
2021-02-13 16:40:54 +0300

    внесены исправления: метод use для прочих url, убраны консольлог, модуль фс, добавлен ретурн в ф-ии обр-ка then

--- controllers/users.js
+++ controllers/users.js
@@ -3,23 +3,20 @@ const getDataFromFile = require('../helpers/files');
 
 const dataPath = path.join(__dirname, '..', 'data', 'users.json');
 
-const getUsers = (req, res) => {
-  return getDataFromFile(dataPath)
-    .then(users => res.status(200).send(users))
-    .catch(err => res.status(500).send(err));
-}
+const getUsers = (req, res) => getDataFromFile(dataPath)
+  .then((users) => res.status(200).send(users))
+  .catch((err) => res.status(500).send(err));
 
-const getProfile = (req, res) => {
-  return getDataFromFile(dataPath)
-    .then(users => {
-      const userBeingRequested = users.find(user => user._id === req.params.id)
-      if (!userBeingRequested) {
-        return res.status(404).send({message: 'Нет пользователя с таким id'});
-      }
+const getProfile = (req, res) => getDataFromFile(dataPath)
+  .then((users) => {
+    const userBeingRequested = users.find((user) => user._id === req.params.id);
+    if (!userBeingRequested) {
+      return res.status(404).send({ message: 'Нет пользователя с таким id' });
+    }
 
-      res.status(200).send(userBeingRequested);
-    })
-    .catch(err => res.status(500).send(err));
-}
+    res.status(200).send(userBeingRequested);
+    return false;
+  })
+  .catch((err) => res.status(500).send(err));
 
-module.exports = {getUsers, getProfile};
+module.exports = { getUsers, getProfile };
